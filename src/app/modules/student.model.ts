60f0bb5ace18ff484803582c006e603f48baa478
Rakibmahmud <hasansaikat74@gmail.com>
2023-11-20 11:01:39 +0600

    done student create features

--- src/app/modules/student.model.ts
+++ src/app/modules/student.model.ts
@@ -1,13 +1,36 @@
-import { Schema, model, connect } from 'mongoose';
-import { Student } from './student/student.interface';
+import { Schema, model } from 'mongoose';
+import {
+  Guardian,
+  LocalGuardian,
+  Student,
+  UserName,
+} from './student/student.interface';
 
-const StudentSchema = new Schema<Student>({
+const userNameSchema = new Schema<UserName>({
+  firstName: { type: String, required: true },
+  middleName: { type: String },
+  lastName: { type: String, required: true },
+});
+
+const guardianSchema = new Schema<Guardian>({
+  fatherName: { type: String, required: true },
+  fatherContact: { type: String, required: true },
+  fatherOccupation: { type: String, required: true },
+  motherName: { type: String, required: true },
+  motherContact: { type: String, required: true },
+  motherOccupation: { type: String, required: true },
+});
+
+const localGuardianSchema = new Schema<LocalGuardian>({
+  name: { type: String, required: true },
+  occupation: { type: String, required: true },
+  contact: { type: String, required: true },
+  address: { type: String, required: true },
+});
+
+const studentSchema = new Schema<Student>({
   id: { type: String },
-  name: {
-    firstName: { type: String, required: true },
-    middleName: { type: String },
-    lastName: { type: String, required: true },
-  },
+  name: userNameSchema,
   gender: ['male', 'female'],
   dateOfBirth: { type: String },
   email: { type: String, required: true },
@@ -16,20 +39,12 @@ const StudentSchema = new Schema<Student>({
   bloodGroup: ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'],
   presentAddress: { type: String, required: true },
   permanentAddress: { type: String, required: true },
-  guardian: {
-    fatherName: { type: String, required: true },
-    fatherContact: { type: String, required: true },
-    fatherOccupation: { type: String, required: true },
-    motherName: { type: String, required: true },
-    motherContact: { type: String, required: true },
-    motherOccupation: { type: String, required: true },
-  },
-  localGuardian: {
-    name: { type: String, required: true },
-    occupation: { type: String, required: true },
-    contact: { type: String, required: true },
-    address: { type: String, required: true },
-  },
+  guardian: guardianSchema,
+  localGuardian: localGuardianSchema,
   profileImage: { type: String },
   isActive: ['active', 'blocked'],
 });
+
+//create model
+
+export const StudentModel = model<Student>('Student', studentSchema);
---
+++ src/app/modules/student/student.controller.ts
@@ -0,0 +1,50 @@
+import { Request, Response } from 'express';
+import { StudentServices } from './student-service';
+
+const createStudent = async (req: Request, res: Response) => {
+  try {
+    const { student: studentData } = req.body;
+    const result = await StudentServices.createStudentIntoDB(studentData);
+
+    res.status(200).json({
+      success: true,
+      message: 'student created successfully',
+      data: result,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const getAllStudent = async (req: Request, res: Response) => {
+  try {
+    const result = await StudentServices.getAllStudentFromDB();
+    res.status(200).json({
+      success: true,
+      message: 'student retrieved successfully',
+      data: result,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const getSingleStudent = async (req: Request, res: Response) => {
+  try {
+    const { studentId } = req.params;
+    const result = await StudentServices.getSingleStudentFromDB(studentId);
+    res.status(200).json({
+      success: true,
+      message: 'student retrieved successfully',
+      data: result,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const StudentControllers = {
+  createStudent,
+  getAllStudent,
+  getSingleStudent,
+};
