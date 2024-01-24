b36daa54ea9b5a19f7a4ced6084d1c17baf80719
Rakibmahmud <hasansaikat74@gmail.com>
2024-01-24 11:42:02 +0600

    added login related api

--- src/app/middleware/authMiddleware.ts
+++ src/app/middleware/authMiddleware.ts
@@ -17,10 +17,17 @@ const auth = (...requiredRoles: TUserRole[]) => {
     }
 
     //check if the token is valid
-    const decoded = jwt.verify(
-      token,
-      config.jwt_access_secret as string,
-    ) as JwtPayload;
+
+    let decoded;
+
+    try {
+      decoded = jwt.verify(
+        token,
+        config.jwt_access_secret as string,
+      ) as JwtPayload;
+    } catch (err) {
+      throw new AppError(httpStatus.UNAUTHORIZED, 'unauthorized');
+    }
 
     const { role, userId, iat } = decoded;
 
